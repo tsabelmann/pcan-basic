c807c24208a5143c7616a400c91163e4c523d175
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-27 11:39:29 +0200

    refact: Moved general trait implementations there

--- src/info.rs
+++ src/info.rs
@@ -1,6 +1,7 @@
 //!
 //!
 
+use crate::channel::Channel;
 use crate::error::{PcanError, PcanOkError};
 use crate::pcan;
 use std::ffi::c_void;
@@ -45,6 +46,46 @@ pub trait ChannelVersion {
     fn channel_version(&self) -> Result<Version, PcanError>;
 }
 
+impl<T: HasChannelVersion + Channel> ChannelVersion for T {
+    fn channel_version(&self) -> Result<Version, PcanError> {
+        let mut data = [0u8; pcan::MAX_LENGTH_VERSION_STRING as usize];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_CHANNEL_VERSION as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => match std::str::from_utf8(&data) {
+                Ok(s) => {
+                    let newlines = s.lines().collect::<Vec<_>>();
+                    if newlines.len() == 4 {
+                        let newlines = newlines
+                            .iter()
+                            .map(|s| s.trim_matches(char::from(0)))
+                            .collect::<Vec<_>>();
+
+                        Ok(Version {
+                            device_driver_name_and_version: String::from(newlines[0]),
+                            architecture: String::from(newlines[1]),
+                            year_of_copyright: String::from(newlines[2]),
+                            company_name_and_city: String::from(newlines[4]),
+                        })
+                    } else {
+                        Err(PcanError::Unknown)
+                    }
+                }
+                Err(_) => Err(PcanError::Unknown),
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
 /* ChannelFeatures trait */
 
 pub(crate) trait HasChannelFeatures {}
@@ -53,6 +94,83 @@ pub trait ChannelFeatures {
     fn is_fd_capable(&self) -> Result<bool, PcanError>;
     fn is_delay_capable(&self) -> Result<bool, PcanError>;
     fn is_io_capable(&self) -> Result<bool, PcanError>;
+}
+
+impl<T: HasChannelFeatures + Channel> ChannelFeatures for T {
+    fn is_fd_capable(&self) -> Result<bool, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_CHANNEL_FEATURES as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => {
+                let value = u32::from_le_bytes(data);
+                if value & pcan::FEATURE_FD_CAPABLE == pcan::FEATURE_FD_CAPABLE {
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+
+    fn is_delay_capable(&self) -> Result<bool, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_CHANNEL_FEATURES as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => {
+                let value = u32::from_le_bytes(data);
+                if value & pcan::FEATURE_DELAY_CAPABLE == pcan::FEATURE_DELAY_CAPABLE {
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+
+    fn is_io_capable(&self) -> Result<bool, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_CHANNEL_FEATURES as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => {
+                let value = u32::from_le_bytes(data);
+                if value & pcan::FEATURE_IO_CAPABLE == pcan::FEATURE_IO_CAPABLE {
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
 }
 
 /* BitrateInfo trait */
