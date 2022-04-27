0945ced8a6559495e48685cd14d4d0a74d513be9
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-27 09:11:57 +0200

    feat: Implmented 5 volts power traits

--- src/can/pcc.rs
+++ src/can/pcc.rs
@@ -6,6 +6,8 @@ use crate::bus::PccBus;
 use crate::can::{Baudrate, HasCanRead, HasCanReadFd, HasCanWrite, HasCanWriteFd, Socket};
 use crate::error::{PcanError, PcanOkError};
 use crate::pcan;
+use crate::special::{FiveVoltsPower, SetFiveVoltsPower};
+use std::ffi::c_void;
 
 #[derive(Debug, PartialEq)]
 pub struct PccCanSocket {
@@ -56,3 +58,57 @@ impl HasCanWrite for PccCanSocket {}
 /* HasCanWriteFd trait implementation */
 
 impl HasCanWriteFd for PccCanSocket {}
+
+/* SPECIAL BEHAVIOR */
+
+/* FiveVoltsPower trait implementation */
+
+impl FiveVoltsPower for PccCanSocket {
+    fn five_volts(&self) -> Result<Bool, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.handle,
+                pcan::PCAN_5VOLTS_POWER as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => {
+                let value = u32::from_le_bytes(data);
+                if value & pcan::PCAN_PARAMETER_ON == pcan::PCAN_PARAMETER_ON {
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+impl SetFiveVoltsPower for PccCanSocket {
+    fn set_five_volts(&self, value: bool) -> Result<(), PcanError> {
+        let mut data = match value {
+            true => pcan::PCAN_PARAMETER_ON.to_le_bytes(),
+            false => pcan::PCAN_PARAMETER_OFF.to_le_bytes(),
+        };
+        let code = unsafe {
+            pcan::CAN_SetValue(
+                self.handle,
+                pcan::PCAN_5VOLTS_POWER as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => Ok(()),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
