5000931f7a140e6c8225bb2081243fdb0f2c9498
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-27 11:34:52 +0200

    refact: Moved implementations there

--- src/hw_ident.rs
+++ src/hw_ident.rs
@@ -1,5 +1,11 @@
-use crate::error::PcanError;
+//!
+//!
+//!
+
+use crate::channel::Channel;
+use crate::error::{PcanError, PcanOkError};
 use crate::pcan;
+use std::ffi::c_void;
 use std::net::Ipv4Addr;
 
 #[derive(Debug, PartialEq)]
@@ -35,125 +41,315 @@ impl TryFrom<u32> for ChannelConditionStatus {
     }
 }
 
+/* ChannelCondition trait */
+
+pub(crate) trait HasChannelCondition {}
+
 pub trait ChannelCondition {
     fn channel_condition(&self) -> Result<ChannelConditionStatus, PcanError>;
 }
 
-pub trait ChannelIdentifying {
-    fn enable_identifying(&self) -> Result<(), PcanError>;
-    fn disable_identifying(&self) -> Result<(), PcanError>;
-    fn identifying(&self, value: bool) -> Result<(), PcanError> {
-        match value {
-            false => self.disable_identifying(),
-            true => self.enable_identifying(),
+impl<T: HasChannelCondition + Channel> ChannelCondition for T {
+    fn channel_condition(&self) -> Result<ChannelConditionStatus, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_CHANNEL_CONDITION as u8,
+                data.as_mut_ptr() as *mut c_void,
+                4,
+            )
+        };
+
+        let value: u32 = u32::from_le_bytes(data);
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => match ChannelConditionStatus::try_from(value) {
+                Ok(status) => Ok(status),
+                Err(_) => Err(PcanError::Unknown),
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
         }
     }
-    fn is_identifying(&self) -> Result<bool, PcanError>;
+}
+
+/* ChannelIdentifying trait */
+
+pub(crate) trait HasChannelIdentifying {}
+
+pub trait ChannelIdentifying {
+    fn is_channel_identifying(&self) -> Result<bool, PcanError>;
+}
+
+impl<T: HasChannelIdentifying + Channel> ChannelIdentifying for T {
+    fn is_channel_identifying(&self) -> Result<bool, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_CHANNEL_IDENTIFYING as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => {
+                let value = u32::from_le_bytes(data);
+                if value & pcan::PCAN_PARAMETER_ON == pcan::PCAN_PARAMETER_ON {
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+pub(crate) trait HasSetChannelIdentifying {}
+
+pub trait SetChannelIdentifying {
+    fn set_channel_identifying(&self, value: bool) -> Result<(), PcanError>;
+}
+
+impl<T: SetChannelIdentifying + Channel> SetChannelIdentifying for T {
+    fn set_channel_identifying(&self, value: bool) -> Result<(), PcanError> {
+        let mut data = match value {
+            true => pcan::PCAN_PARAMETER_ON.to_le_bytes(),
+            false => pcan::PCAN_PARAMETER_OFF.to_le_bytes(),
+        };
+        let code = unsafe {
+            pcan::CAN_SetValue(
+                self.channel(),
+                pcan::PCAN_CHANNEL_IDENTIFYING as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => Ok(()),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
 }
 
 /* Device Id */
 
+pub(crate) trait HasDeviceId {}
+
 pub trait DeviceId {
     fn device_id(&self) -> Result<u32, PcanError>;
 }
+
+impl<T: HasDeviceId + Channel> DeviceId for T {
+    fn device_id(&self) -> Result<u32, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_DEVICE_ID as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => Ok(u32::from_le_bytes(data)),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+pub(crate) trait HasSetDeviceId {}
 
 pub trait SetDeviceId {
     type Item;
     fn set_device_id(&self, value: Self::Item) -> Result<(), PcanError>;
 }
 
-// impl<T: HasSetDeviceId + InitializedChannel> SetDeviceId for T {
-//     type Item = u32;
-//     fn set_device_id(&self, value: Self::Item) -> Result<(), PcanError> {
-//         let mut data = value.to_le_bytes();
-//         let code = unsafe {
-//             pcan::CAN_SetValue(
-//                 self.initialized_channel(),
-//                 pcan::PCAN_DEVICE_ID as u8,
-//                 data.as_mut_ptr() as *mut c_void,
-//                 data.len() as u32,
-//             )
-//         };
-//
-//         match PcanOkError::try_from(code) {
-//             Ok(PcanOkError::Ok) => Ok(()),
-//             Ok(PcanOkError::Err(err)) => Err(err),
-//             Err(_) => Err(PcanError::Unknown),
-//         }
-//     }
-// }
+impl<T: HasSetDeviceId + Channel> SetDeviceId for T {
+    type Item = u32;
+    fn set_device_id(&self, value: Self::Item) -> Result<(), PcanError> {
+        let mut data = value.to_le_bytes();
+        let code = unsafe {
+            pcan::CAN_SetValue(
+                self.channel(),
+                pcan::PCAN_DEVICE_ID as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => Ok(()),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
 
 /* Hardware Name */
+
+pub(crate) trait HasHardwareName {}
 
 pub trait HardwareName {
     fn hardware_name(&self) -> Result<String, PcanError>;
 }
 
+impl<T: HasHardwareName + Channel> HardwareName for T {
+    fn hardware_name(&self) -> Result<String, PcanError> {
+        let mut data = [0u8; pcan::MAX_LENGTH_HARDWARE_NAME as usize];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_HARDWARE_NAME as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => match std::str::from_utf8(&data) {
+                Ok(s) => {
+                    let s = s.trim_matches(char::from(0));
+                    Ok(String::from(s))
+                }
+                Err(_) => Err(PcanError::Unknown),
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
 /* Controller Number */
+
+pub(crate) trait HasControllerNumber {}
 
 pub trait ControllerNumber {
     fn controller_number(&self) -> Result<u32, PcanError>;
 }
+
+impl<T: HasControllerNumber + Channel> ControllerNumber for T {
+    fn controller_number(&self) -> Result<u32, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_CONTROLLER_NUMBER as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => Ok(u32::from_le_bytes(data)),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+pub(crate) trait HasSetControllerNumber {}
 
 pub trait SetControllerNumber {
     type Item;
     fn set_controller_number(&self, value: Self::Item) -> Result<(), PcanError>;
 }
 
-// impl<T: HasSetControllerNumber + InitializedChannel> SetControllerNumber for T {
-//     type Item = u32;
-//     fn set_controller_number(&self, value: Self::Item) -> Result<(), PcanError> {
-//         let mut data = value.to_le_bytes();
-//         let code = unsafe {
-//             pcan::CAN_SetValue(
-//                 self.initialized_chanwnel(),
-//                 pcan::PCAN_CONTROLLER_NUMBER as u8,
-//                 data.as_mut_ptr() as *mut c_void,
-//                 data.len() as u32,
-//             )
-//         };
-//
-//         match PcanOkError::try_from(code) {
-//             Ok(PcanOkError::Ok) => Ok(()),
-//             Ok(PcanOkError::Err(err)) => Err(err),
-//             Err(_) => Err(PcanError::Unknown),
-//         }
-//     }
-// }
+impl<T: HasSetControllerNumber + Channel> SetControllerNumber for T {
+    type Item = u32;
+    fn set_controller_number(&self, value: Self::Item) -> Result<(), PcanError> {
+        let mut data = value.to_le_bytes();
+        let code = unsafe {
+            pcan::CAN_SetValue(
+                self.channel(),
+                pcan::PCAN_CONTROLLER_NUMBER as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
 
-trait IpAddress {
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => Ok(()),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+/* IpAddress trait */
+
+pub(crate) trait HasIpAddress {}
+
+pub trait IpAddress {
     fn ip_address(&self) -> Result<Ipv4Addr, PcanError>;
 }
 
-// impl<T: HasIpAddress + InitializedChannel> IpAddress for T {
-//     fn ip_address(&self) -> Result<Ipv4Addr, PcanError> {
-//         let mut data = [0u8; 20];
-//         let code = unsafe {
-//             pcan::CAN_GetValue(
-//                 self.handle,
-//                 pcan::PCAN_IP_ADDRESS as u8,
-//                 data.as_mut_ptr() as *mut c_void,
-//                 data.len() as u32,
-//             )
-//         };
-//
-//         match PcanOkError::try_from(code) {
-//             Ok(PcanOkError::Ok) => match std::str::from_utf8(&data) {
-//                 Ok(s) => {
-//                     let s = s.trim_matches(char::from(0));
-//                     match s.parse() {
-//                         Ok(ip) => Ok(ip),
-//                         Err(_) => Err(PcanError::Unknown),
-//                     }
-//                 }
-//                 Err(_) => Err(PcanError::Unknown),
-//             },
-//             Ok(PcanOkError::Err(err)) => Err(err),
-//             _ => Err(PcanError::Unknown),
-//         }
-//     }
-// }
+impl<T: HasIpAddress + Channel> IpAddress for T {
+    fn ip_address(&self) -> Result<Ipv4Addr, PcanError> {
+        let mut data = [0u8; 20];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_IP_ADDRESS as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => match std::str::from_utf8(&data) {
+                Ok(s) => {
+                    let s = s.trim_matches(char::from(0));
+                    match s.parse() {
+                        Ok(ip) => Ok(ip),
+                        Err(_) => Err(PcanError::Unknown),
+                    }
+                }
+                Err(_) => Err(PcanError::Unknown),
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            _ => Err(PcanError::Unknown),
+        }
+    }
+}
+
+/* DevicePartNumber trait */
+
+pub(crate) trait HasDevicePartNumber {}
 
 pub trait DevicePartNumber {
     fn device_part_number(&self) -> Result<String, PcanError>;
+}
+
+impl<T: HasDevicePartNumber + Channel> DevicePartNumber for T {
+    fn device_part_number(&self) -> Result<String, PcanError> {
+        let mut data = [0u8; 100];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel(),
+                pcan::PCAN_DEVICE_PART_NUMBER as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32,
+            )
+        };
+
+        match PcanOkError::try_from(code) {
+            Ok(PcanOkError::Ok) => match std::str::from_utf8(&data) {
+                Ok(s) => {
+                    let s = s.trim_matches(char::from(0));
+                    Ok(String::from(s))
+                }
+                Err(_) => Err(PcanError::Unknown),
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
 }
