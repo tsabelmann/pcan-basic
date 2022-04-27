35f1370b28f4eb4cc1de955edff1eed078f42290
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-27 10:17:07 +0200

    tests: Added tests

--- src/can.rs
+++ src/can.rs
@@ -482,3 +482,82 @@ impl<T: Socket + HasCanWriteFd> CanWriteFd for T {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_frame_new_001() {
+        let can_frame_1 =
+            CanFrame::new(0x20, MessageType::Standard, &[0, 1, 2, 3, 4, 5, 6, 7]).unwrap();
+
+        let can_frame_2 =
+            CanFrame::new(0x20, MessageType::Standard, &[0, 1, 2, 3, 4, 5, 6, 7]).unwrap();
+
+        assert_eq!(can_frame_1, can_frame_2);
+    }
+
+    #[test]
+    fn can_frame_new_002() {
+        let can_frame_1 =
+            CanFrame::new(0x20, MessageType::Extended, &[0, 1, 2, 3, 4, 5, 6, 7]).unwrap();
+
+        let can_frame_2 =
+            CanFrame::new(0x20, MessageType::Extended, &[0, 1, 2, 3, 4, 5, 6, 7]).unwrap();
+
+        assert_eq!(can_frame_1, can_frame_2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn can_frame_new_003() {
+        let _can_frame_1 =
+            CanFrame::new(0x20, MessageType::Standard, &[0, 1, 2, 3, 4, 5, 6, 7, 8]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn can_frame_new_004() {
+        let _can_frame_1 =
+            CanFrame::new(0x20, MessageType::Extended, &[0, 1, 2, 3, 4, 5, 6, 7, 8]).unwrap();
+    }
+
+    /* CAN FD FRAME */
+
+    #[test]
+    fn can_fd_frame_new_001() {
+        let can_frame_1 =
+            CanFdFrame::new(0x20, MessageType::Standard, &(0..64u8).collect::<Vec<_>>()).unwrap();
+
+        let can_frame_2 =
+            CanFdFrame::new(0x20, MessageType::Standard, &(0..64u8).collect::<Vec<_>>()).unwrap();
+
+        assert_eq!(can_frame_1, can_frame_2);
+    }
+
+    #[test]
+    fn can_fd_frame_new_002() {
+        let can_frame_1 =
+            CanFdFrame::new(0x20, MessageType::Extended, &(0..64u8).collect::<Vec<_>>()).unwrap();
+
+        let can_frame_2 =
+            CanFdFrame::new(0x20, MessageType::Extended, &(0..64u8).collect::<Vec<_>>()).unwrap();
+
+        assert_eq!(can_frame_1, can_frame_2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn can_fd_frame_new_003() {
+        let _can_frame_1 =
+            CanFdFrame::new(0x20, MessageType::Standard, &(0..65u8).collect::<Vec<_>>()).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn can_fd_frame_new_004() {
+        let _can_frame_1 =
+            CanFrame::new(0x20, MessageType::Extended, &(0..65u8).collect::<Vec<_>>()).unwrap();
+    }
+}
