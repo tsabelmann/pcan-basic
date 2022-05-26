f57d8eb86458b894d5f58252add40c312cb7642f
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-26 23:18:13 +0200

    init: Added bitrate_info_2.rs

---
+++ examples/bitrate_info_2.rs
@@ -0,0 +1,23 @@
+use pcan_basic::bus::UsbBus;
+use pcan_basic::error::PcanError;
+use pcan_basic::info::BitrateInfo;
+use pcan_basic::socket::Baudrate;
+use pcan_basic::socket::usb::UsbCanSocket;
+
+fn main() {
+    let can_socket = match UsbCanSocket::open(UsbBus::USB1, Baudrate::Baud500K) {
+        Ok(can_socket) => { can_socket }
+        Err(err) => {
+            println!("{:?}", err);
+            return;
+        }
+    };
+
+    match can_socket.bitrate_info() {
+        Ok((btr0, btr1)) => {
+            println!("btr0={}", btr0);
+            println!("btr1={}", btr1);
+        },
+        Err(err) => println!("{:?}", err)
+    }
+}
