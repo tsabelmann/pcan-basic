7be5015facf21d1bb72c7834c69a33786e71aa64
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-26 15:11:25 +0200

    feat: Implemented DeviceId trait

--- src/bus/pci.rs
+++ src/bus/pci.rs
@@ -1,6 +1,6 @@
 use crate::bus::Bus;
 use crate::channel::Channel;
-use crate::hw::HasChannelCondition;
+use crate::hw::{HasChannelCondition, HasDeviceId};
 use crate::pcan;
 
 ///
@@ -109,5 +109,7 @@ impl Channel for PciBus {
 /* HARDWARE IDENTIFICATION */
 
 impl HasChannelCondition for PciBus {}
+
+impl HasDeviceId for PciBus {}
 
 /* SPECIAL BEHAVIOR */
