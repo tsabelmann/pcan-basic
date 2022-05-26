58efb84a609ff1afb4ac1d5cd3c3a2e718664115
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-26 22:02:18 +0200

    feat: Implemented DevicePartNumber trait

--- src/socket/pci.rs
+++ src/socket/pci.rs
@@ -6,7 +6,7 @@ use crate::bus::PciBus;
 use crate::socket::{Baudrate, HasCanRead, HasCanReadFd, HasCanWrite, HasCanWriteFd, Socket};
 use crate::channel::Channel;
 use crate::error::{PcanError, PcanOkError};
-use crate::hw::{HasControllerNumber, HasDeviceId, HasHardwareName, HasSetControllerNumber, HasSetDeviceId};
+use crate::hw::{HasControllerNumber, HasDeviceId, HasDevicePartNumber, HasHardwareName, HasSetControllerNumber, HasSetDeviceId};
 use crate::pcan;
 
 #[derive(Debug, PartialEq)]
@@ -67,5 +67,7 @@ impl HasHardwareName for PciCanSocket {}
 
 impl HasControllerNumber for PciCanSocket {}
 impl HasSetControllerNumber for PciCanSocket {}
+
+impl HasDevicePartNumber for PciCanSocket {}
 
 /* SPECIAL BEHAVIOR */
