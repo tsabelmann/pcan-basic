b19f26c3c83608e871b0ee2191d3eb0669a3268e
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-26 19:42:08 +0200

    feat: Implemented HardwareName trait

--- src/bus/pcc.rs
+++ src/bus/pcc.rs
@@ -1,6 +1,6 @@
 use crate::bus::Bus;
 use crate::channel::Channel;
-use crate::hw::HasChannelCondition;
+use crate::hw::{HasChannelCondition, HasHardwareName};
 use crate::pcan;
 
 ///
@@ -53,5 +53,7 @@ impl Channel for PccBus {
 /* HARDWARE IDENTIFICATION */
 
 impl HasChannelCondition for PccBus {}
+
+impl HasHardwareName for PccBus {}
 
 /* SPECIAL BEHAVIOR */
