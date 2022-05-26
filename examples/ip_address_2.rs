e4457e3af9f085853d926d5a020103be1727a11c
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-26 20:36:08 +0200

    init: Added ip_address_2.rs

---
+++ examples/ip_address_2.rs
@@ -0,0 +1,21 @@
+use pcan_basic::bus::LanBus;
+use pcan_basic::error::PcanError;
+use pcan_basic::hw::{HardwareName, IpAddress};
+use pcan_basic::socket::Baudrate;
+use pcan_basic::socket::lan::LanCanSocket;
+
+fn main() {
+    let can_socket = LanCanSocket::open(LanBus::LAN1,
+                                    Baudrate::Baud500K);
+
+    let can_socket = match can_socket {
+        Ok(socket) => { socket }
+        Err(_) => { return; }
+    };
+
+    let result = can_socket.ip_address();
+    match result {
+        Ok(ip_address) => println!("{}", ip_address),
+        _ => println!("An error occurred!")
+    }
+}
