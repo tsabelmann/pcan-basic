850f2e310cef1dfd657fff4b8d1eb0a82ded2b6e
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-26 22:34:39 +0200

    init: Added channel_version_2.rs

---
+++ examples/channel_version_2.rs
@@ -0,0 +1,22 @@
+use pcan_basic::bus::UsbBus;
+use pcan_basic::error::PcanError;
+use pcan_basic::hw::{HardwareName};
+use pcan_basic::info::ChannelVersion;
+use pcan_basic::socket::Baudrate;
+use pcan_basic::socket::usb::UsbCanSocket;
+
+fn main() {
+    let can_socket = UsbCanSocket::open(UsbBus::USB1,
+                                    Baudrate::Baud500K);
+
+    let can_socket = match can_socket {
+        Ok(socket) => { socket }
+        Err(_) => { return; }
+    };
+
+    let result = can_socket.channel_version();
+    match result {
+        Ok(version) => println!("{:?}", version),
+        Err(err) => println!("{:?}", err)
+    }
+}
