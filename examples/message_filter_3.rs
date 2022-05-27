302bb280a8beebe94077bba5c9ad6ad33d07a648
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-27 16:35:07 +0200

    init: Added message_filter_3.rs

---
+++ examples/message_filter_3.rs
@@ -0,0 +1,31 @@
+use pcan_basic::bus::UsbBus;
+use pcan_basic::df::SetMessageFilter;
+use pcan_basic::socket::usb::UsbCanSocket;
+use pcan_basic::socket::Baudrate;
+use pcan_basic::socket::CanRead;
+
+fn main() {
+    let can_socket = match UsbCanSocket::open(UsbBus::USB1, Baudrate::Baud500K) {
+        Ok(can_socket) => can_socket,
+        Err(err) => {
+            println!("{:?}", err);
+            return;
+        }
+    };
+
+    match can_socket.set_open_filter() {
+        Ok(_) => {}
+        Err(err) => println!("{:?}", err),
+    }
+
+    loop {
+        let can_frame = can_socket.read();
+        match can_frame {
+            Ok((frame, timestamp)) => {
+                println!("{:?}", frame);
+                println!("{:?}", timestamp);
+            }
+            Err(_) => {}
+        }
+    }
+}
