b411413e54d710859d4997364d1123c9532c8255
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-27 11:30:18 +0200

    feat: Implmented BitrateInfoFd trait

--- src/bus/dng.rs
+++ src/bus/dng.rs
@@ -1,7 +1,7 @@
 use crate::bus::Bus;
 use crate::channel::Channel;
 use crate::hw::{HasChannelCondition, HasControllerNumber, HasDevicePartNumber, HasHardwareName};
-use crate::info::{HasBitrateInfo, HasChannelFeatures, HasChannelVersion};
+use crate::info::{HasBitrateInfo, HasBitrateInfoFd, HasChannelFeatures, HasChannelVersion};
 use crate::pcan;
 
 ///
@@ -64,5 +64,7 @@ impl HasChannelVersion for DngBus {}
 impl HasChannelFeatures for DngBus {}
 
 impl HasBitrateInfo for DngBus {}
+
+impl HasBitrateInfoFd for DngBus {}
 
 /* SPECIAL BEHAVIOR */
