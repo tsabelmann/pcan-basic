e3f5927749e5096ca7986cd453a198332a804e26
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-27 11:40:34 +0200

    feat: Implemented NominalBusSpeed trait

--- src/socket/pcc.rs
+++ src/socket/pcc.rs
@@ -7,7 +7,7 @@ use crate::socket::{Baudrate, HasCanRead, HasCanReadFd, HasCanWrite, HasCanWrite
 use crate::channel::Channel;
 use crate::error::{PcanError, PcanOkError};
 use crate::hw::{HasControllerNumber, HasDevicePartNumber, HasHardwareName, HasSetControllerNumber};
-use crate::info::{HasBitrateInfo, HasChannelFeatures, HasChannelVersion};
+use crate::info::{HasBitrateInfo, HasChannelFeatures, HasChannelVersion, HasNominalBusSpeed};
 use crate::pcan;
 
 #[derive(Debug, PartialEq)]
@@ -75,5 +75,7 @@ impl HasChannelVersion for PccCanSocket {}
 impl HasChannelFeatures for PccCanSocket {}
 
 impl HasBitrateInfo for PccCanSocket {}
+
+impl HasNominalBusSpeed for PccCanSocket {}
 
 /* SPECIAL BEHAVIOR */
