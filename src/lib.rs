10ce06fc96814c35b42813983eaf96e596e455fe
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-27 18:44:41 +0200

    feat: Changed data flow module

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,12 @@
 
 #[warn(dead_code)]
 pub mod bus;
-pub mod socket;
 mod channel;
+pub mod df;
 pub mod error;
 pub mod hw;
 pub mod info;
+pub mod socket;
 pub mod special;
 
 use pcan_basic_sys as pcan;
