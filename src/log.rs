08b0a09788450608fe38f5a8d2f80a9013e6df9f
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-28 11:30:29 +0200

    feat: Implemented log_configure

--- src/log.rs
+++ src/log.rs
@@ -97,3 +97,92 @@ pub fn set_logging(enable: bool) -> Result<(), PcanError> {
         Err(_) => Err(PcanError::Unknown),
     }
 }
+
+///
+///
+#[derive(PartialEq, Debug)]
+pub enum LogFunction {
+    /// Default
+    Default,
+    ///
+    Entry,
+    ///
+    Parameters,
+    ///
+    Leave,
+    ///
+    Write,
+    ///
+    Read,
+}
+
+impl TryFrom<u32> for LogFunction {
+    type Error = ();
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        match value {
+            pcan::LOG_FUNCTION_DEFAULT => Ok(LogFunction::Default),
+            pcan::LOG_FUNCTION_ENTRY => Ok(LogFunction::Entry),
+            pcan::LOG_FUNCTION_PARAMETERS => Ok(LogFunction::Parameters),
+            pcan::LOG_FUNCTION_LEAVE => Ok(LogFunction::Leave),
+            pcan::LOG_FUNCTION_WRITE => Ok(LogFunction::Write),
+            pcan::LOG_FUNCTION_READ => Ok(LogFunction::Read),
+            _ => Err(()),
+        }
+    }
+}
+
+impl From<LogFunction> for u32 {
+    fn from(value: LogFunction) -> Self {
+        match value {
+            LogFunction::Default => pcan::LOG_FUNCTION_DEFAULT,
+            LogFunction::Entry => pcan::LOG_FUNCTION_ENTRY,
+            LogFunction::Parameters => pcan::LOG_FUNCTION_PARAMETERS,
+            LogFunction::Leave => pcan::LOG_FUNCTION_LEAVE,
+            LogFunction::Write => pcan::LOG_FUNCTION_WRITE,
+            LogFunction::Read => pcan::LOG_FUNCTION_READ,
+        }
+    }
+}
+
+pub fn log_configuration() -> Result<LogFunction, PcanError> {
+    let mut data = [0u8; 4];
+    let code = unsafe {
+        pcan::CAN_GetValue(
+            pcan::PCAN_NONEBUS as u16,
+            pcan_basic_sys::PCAN_LOG_CONFIGURE as u8,
+            data.as_mut_ptr() as *mut c_void,
+            data.len() as u32,
+        )
+    };
+
+    match PcanOkError::try_from(code) {
+        Ok(PcanOkError::Ok) => {
+            let code = u32::from_le_bytes(data);
+            match LogFunction::try_from(code) {
+                Ok(log_config) => Ok(log_config),
+                Err(_) => Err(PcanError::Unknown),
+            }
+        }
+        Ok(PcanOkError::Err(err)) => Err(err),
+        Err(_) => Err(PcanError::Unknown),
+    }
+}
+
+pub fn configure_log(config: LogFunction) -> Result<(), PcanError> {
+    let mut data = u32::from(config).to_le_bytes();
+    let code = unsafe {
+        pcan::CAN_SetValue(
+            pcan::PCAN_NONEBUS as u16,
+            pcan_basic_sys::PCAN_LOG_CONFIGURE as u8,
+            data.as_mut_ptr() as *mut c_void,
+            data.len() as u32,
+        )
+    };
+
+    match PcanOkError::try_from(code) {
+        Ok(PcanOkError::Ok) => Ok(()),
+        Ok(PcanOkError::Err(err)) => Err(err),
+        Err(_) => Err(PcanError::Unknown),
+    }
+}
