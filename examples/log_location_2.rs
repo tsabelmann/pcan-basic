dfd15434bc2c83acce0bd2cf0ca358f18e9d96c8
tsabelmann <tim.sabelmann@hotmail.de>
2023-02-25 15:51:07 +0100

    feat: Updated log_location_2.rs

--- examples/log_location_2.rs
+++ examples/log_location_2.rs
@@ -1,7 +1,13 @@
+use std::path::PathBuf;
 use pcan_basic::log::{log_location, set_log_location};
 
 fn main() {
-    match set_log_location("/home/tsa") {
+    match log_location() {
+        Ok(path) => println!("{:?}", path),
+        Err(err) => println!("{:?}", err),
+    }
+
+    match set_log_location(r"/home") {
         Ok(_) => {}
         Err(err) => println!("{:?}", err),
     }
