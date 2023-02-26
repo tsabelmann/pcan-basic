5ab45a727e72430f781229a4c6d2657a27cd13a4
tsabelmann <tim.sabelmann@hotmail.de>
2023-02-26 20:07:18 +0100

    feat: Updated log_location_3.rs example

--- examples/log_location_3.rs
+++ examples/log_location_3.rs
@@ -1,11 +1,31 @@
-use pcan_basic::log::{log_location, set_default_log_location};
+use pcan_basic::log::{log_location, set_log_location, set_default_log_location};
 
 fn main() {
-    match set_default_log_location() {
-        Ok(_) => {}
+    println!("Get current path");
+    match log_location() {
+        Ok(path) => println!("{:?}", path),
         Err(err) => println!("{:?}", err),
     }
 
+    println!("Set path - not the default");
+    match set_log_location("/home/") {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Get current path");
+    match log_location() {
+        Ok(path) => println!("{:?}", path),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Reset the path to the default");
+    match set_default_log_location() {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Get current path");
     match log_location() {
         Ok(path) => println!("{:?}", path),
         Err(err) => println!("{:?}", err),
