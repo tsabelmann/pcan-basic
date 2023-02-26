73d8a2bb219e0192efcf1cf2a8ea4bf946ddedfb
tsabelmann <tim.sabelmann@hotmail.de>
2023-02-26 18:45:43 +0100

    feat: Updated log_location_2.rs example - it now shows the default, set, and reset behavior

--- examples/log_location_2.rs
+++ examples/log_location_2.rs
@@ -2,12 +2,25 @@ use std::path::PathBuf;
 use pcan_basic::log::{log_location, set_log_location};
 
 fn main() {
+    println!("First attempt");
     match log_location() {
         Ok(path) => println!("{:?}", path),
         Err(err) => println!("{:?}", err),
     }
 
-    match set_log_location(r"/home") {
+    println!("Second attempt");
+    match set_log_location("/home/tsa/") {
+        Ok(_) => {}
+        Err(err) => println!("{:?}", err),
+    }
+
+    match log_location() {
+        Ok(path) => println!("{:?}", path),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Third attempt");
+    match set_log_location("") {
         Ok(_) => {}
         Err(err) => println!("{:?}", err),
     }
