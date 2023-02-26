e36f840610d80898b0cbde0136a6651d00796e59
tsabelmann <tim.sabelmann@hotmail.de>
2023-02-26 20:16:08 +0100

    feat: Updated log_status_2.rs example

--- examples/log_status_2.rs
+++ examples/log_status_2.rs
@@ -1,23 +1,33 @@
 use pcan_basic::log::{is_logging, set_logging};
 
 fn main() {
-    match set_logging(false) {
-        Ok(_) => {}
-        Err(err) => println!("{:?}", err),
-    }
-
+    println!("Get current logging status");
     match is_logging() {
-        Ok(is_logging) => println!("is_logging={}", is_logging),
+        Ok(is_logging) => println!("Is logging: {}", is_logging),
         Err(err) => println!("{:?}", err),
     }
 
+    println!("Set logging active!");
     match set_logging(true) {
-        Ok(_) => {}
+        Ok(_) => println!("Successful!"),
         Err(err) => println!("{:?}", err),
     }
 
+    println!("Get current logging status");
     match is_logging() {
-        Ok(is_logging) => println!("is_logging={}", is_logging),
+        Ok(is_logging) => println!("Is logging: {}", is_logging),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Set logging inactive!");
+    match set_logging(false) {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Get current logging status");
+    match is_logging() {
+        Ok(is_logging) => println!("Is logging: {}", is_logging),
         Err(err) => println!("{:?}", err),
     }
 }
