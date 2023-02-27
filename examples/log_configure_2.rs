50a49f7fa9fa85a1d220c59abe988e29f2108419
tsabelmann <tim.sabelmann@hotmail.de>
2023-02-27 19:15:26 +0100

    feat: Updated log_configure_2.rs example

--- examples/log_configure_2.rs
+++ examples/log_configure_2.rs
@@ -1,4 +1,4 @@
-use pcan_basic::log::{configure_log, log_configuration, LogFunction};
+use pcan_basic::log::{configure_log, is_logging, log_configuration, LogFunction, set_logging};
 
 fn main() {
     match configure_log(LogFunction::Write) {
@@ -14,6 +14,16 @@ fn main() {
     match configure_log(LogFunction::Parameters) {
         Ok(_) => {}
         Err(err) => println!("{:?}", err),
+    }
+
+    match is_logging() {
+        Ok(logging) => println!("Is logging: {}", logging),
+        Err(err) => println!("The error: {:?}", err)
+    }
+
+    match set_logging(true) {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("The error: {:?}", err)
     }
 
     match log_configuration() {
