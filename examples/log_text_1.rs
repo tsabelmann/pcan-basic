6e21a5f4730635ae32417ab6df3940545e356ca0
tsabelmann <tim.sabelmann@hotmail.de>
2023-02-27 19:08:17 +0100

    feat: Updated example log_text_1.rs

--- examples/log_text_1.rs
+++ examples/log_text_1.rs
@@ -1,8 +1,45 @@
-use pcan_basic::log::log_text;
+use pcan_basic::log::{insert_log_text, is_logging, set_logging};
 
 fn main() {
-    match log_text("Hello World") {
-        Ok(_) => {}
+    println!("Get current logging status");
+    match is_logging() {
+        Ok(is_logging) => println!("Is logging: {}", is_logging),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Set logging active!");
+    match set_logging(true) {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Get current logging status");
+    match is_logging() {
+        Ok(is_logging) => println!("Is logging: {}", is_logging),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Insert text 1");
+    match insert_log_text("Hello World 1") {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Insert text 2");
+    match insert_log_text("Hello World 2") {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Set logging inactive!");
+    match set_logging(false) {
+        Ok(_) => println!("Successful!"),
+        Err(err) => println!("{:?}", err),
+    }
+
+    println!("Get current logging status");
+    match is_logging() {
+        Ok(is_logging) => println!("Is logging: {}", is_logging),
         Err(err) => println!("{:?}", err),
     }
 }
